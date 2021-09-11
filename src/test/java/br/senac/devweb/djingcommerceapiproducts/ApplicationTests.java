01602c030998eff22f70ed25b3830899c2893858
Alexandrebor <alexandreblosborges@gmail.com>
2021-09-10 21:03:46 -0300

    setup inicial

---
+++ src/test/java/br/senac/devweb/djingcommerceapiproducts/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.devweb.djingcommerceapiproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
